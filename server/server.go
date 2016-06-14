e1f6da64de7f19a28e155d14ade6fcd58d3d2fb2
richardlt <richard.le.terrier@gmail.com>
2016-06-14 19:54:39 +0200

    Create basic server

--- server/server.go
+++ server/server.go
@@ -1,1 +1,15 @@
 package server
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/engine/standard"
+	"github.com/labstack/echo/middleware"
+)
+
+// Start server
+func Start() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Run(standard.New(":" + port))
+}
