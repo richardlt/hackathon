5f313f3a8be04bc20f4402b4b2857875dce9e9d1
Mathieu Cornic <mathieu.cornic@gmail.com>
2016-06-14 21:26:42 +0200

    add register from slave

--- slave/slave.go
+++ slave/slave.go
@@ -1,15 +1,22 @@
 package slave
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/engine/standard"
 	"github.com/labstack/echo/middleware"
+	"github.com/richardlt/hackathon/types"
 )
 
 // Serve slave
 func Serve() {
+
+	master := "http://localhost:8081/"
+
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
@@ -17,6 +24,18 @@ func Serve() {
 	e.Get("/answer", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, nil)
 	})
+
+	u := types.Register{Url: master}
+	b := new(bytes.Buffer)
+	json.NewEncoder(b).Encode(u)
+	res, err := http.Post("http://localhost:8080/register", "application/json; charset=utf-8", b)
+	if err != nil {
+		fmt.Println("Can't register to master : " + master)
+		panic(err)
+	} else {
+		fmt.Println("Sent register to master : " + master)
+	}
+	defer res.Body.Close()
 
 	e.Run(standard.New(":8081"))
 }
