573b6b6a2a53322c690b36065210a6e6520b0215
Leclerc Gwendal <gwendal.leclerc@soprasteria.com>
2016-06-14 20:56:04 +0200

    feat: add get random question + load questions from file

--- types/questions/model.go
+++ types/questions/model.go
@@ -1,5 +1,13 @@
 package questions
 
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"math/rand"
+	"os"
+)
+
 //Questions question list
 type Questions []Question
 
@@ -10,6 +18,26 @@ type Question struct {
 	Answer string `json:"answer"`
 }
 
-func (qs Questions) GetRandomQuestion() (*Question, error) {
-	return nil, nil
+func (qs Questions) GetRandomQuestion() (Question, error) {
+	size := len(qs)
+	if size <= 0 {
+		return Question{}, errors.New("No questions found.")
+	}
+	index := rand.Intn(size)
+	return qs[index], nil
+
+}
+
+func (qs *Questions) Load(path string) error {
+
+	configFile, err := os.Open(path)
+	if err != nil {
+		return fmt.Errorf("Error opening questions file: %v", err.Error())
+	}
+
+	jsonParser := json.NewDecoder(configFile)
+	if err = jsonParser.Decode(&qs); err != nil {
+		return fmt.Errorf("Error parsing questions file", err.Error())
+	}
+	return nil
 }
